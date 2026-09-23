0754825ccc8499985d6ccab89e43cca9f0ddc874
georgi-cole <georgi.val.kolev@gmail.com>
2026-09-24 00:03:17 +0300

    Temporarily allow Back 2 the Game Capitalization QA preview

--- src/screens/MinigameLab/MinigameLab.tsx
+++ src/screens/MinigameLab/MinigameLab.tsx
@@ -91,6 +91,7 @@ export default function MinigameLab({ keyboardGamesOnly = false }: MinigameLabPr
   const [isMinimized, setIsMinimized] = useState(false)
 
   const freezeEnabled = getRouteFlag('freeze')
+  const capitalizationContext = params.get('context') === 'battleBack' ? 'battleBack' : undefined
   const selectedGame =
     activeGames.find((game) => game.key === selectedGameKey) ?? activeGames[0] ?? null
   const participants = useMemo(() => buildParticipants(playerCount), [playerCount])
@@ -151,7 +152,7 @@ export default function MinigameLab({ keyboardGamesOnly = false }: MinigameLabPr
           <MinigameHost
             key={previewKey}
             game={selectedGame}
-            gameOptions={{ seed }}
+            gameOptions={{ seed, capitalizationContext }}
             participants={participants}
             skipRules={skipRules}
             skipCountdown={skipCountdown}
