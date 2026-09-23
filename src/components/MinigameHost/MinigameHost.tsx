9c7af3c6badf9da21ef09637538396eea9be2215
georgi-cole <georgi.val.kolev@gmail.com>
2026-09-24 00:03:00 +0300

    Temporarily expose Capitalization context in QA host

--- src/components/MinigameHost/MinigameHost.tsx
+++ src/components/MinigameHost/MinigameHost.tsx
@@ -760,12 +760,20 @@ export default function MinigameHost({
     }
 
     if (game.implementation === 'react' && game.reactComponentKey === 'Capitalization') {
-      const CapitalizationComp = reactComponents.Capitalization
+      const CapitalizationComp = reactComponents.Capitalization as React.ComponentType<
+        GenericMinigameProps & { context?: 'loh' | 'battleBack' }
+      >
+      const capitalizationContext =
+        gameOptions.capitalizationContext === 'battleBack' ? 'battleBack' : 'loh'
+      const capitalizationSeed =
+        typeof gameOptions.seed === 'number' ? gameOptions.seed : undefined
       return (
         <CapitalizationComp
           autoStart={true}
           participantIds={participantIds}
           participants={participants}
+          seed={capitalizationSeed}
+          context={capitalizationContext}
           onFinish={(
             value: number,
             tiebreakerMs?: number,
